c23adbd7563f9e6428ef457a33c03da8091cd0e8
AGT Studio <tan.ahmet.at@gmail.com>
2026-09-07 07:00:35 +0300

    fix: cache runtime app and curriculum assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='speakio-v2';
-const CORE=['./','./index.html','./manifest.json','./content/a1-curriculum.json','./content/content-loader.js'];
+const CACHE='speakio-v3';
+const CORE=['./','./index.html','./app.js','./manifest.json','./content/a1-curriculum.json','./content/content-loader.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>e.request.mode==='navigate'?caches.match('./index.html'):undefined)))});
