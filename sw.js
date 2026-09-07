84482f1338f74d6ad8f8d245e47b0bb4e21addaf
AGT Studio <tan.ahmet.at@gmail.com>
2026-09-07 07:45:24 +0300

    chore: bump service worker cache v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='speakio-v8';
+const CACHE='speakio-v9';
 const CORE=['./','./index.html','./app.js','./runtime-fixes.js','./build-fix.js','./premium.js','./manifest.json','./content/a1-curriculum.json','./content/content-loader.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
